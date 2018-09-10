5812d41dc0c79c180ab634c1c02583548196f2cd
wilfredopacheco <wilfredo.pacheco.sa@gmail.com>
2018-09-10 15:24:02 -0500

    half way through the exercise.

---
+++ js/promises.js
@@ -0,0 +1,11 @@
+const wait = (num) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, num)
+    });
+
+};
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
